6a41ef6d680ad255599f827b6c6f65cfead06cb6
nomorewzx <nomorewzx@163.com>
2020-11-22 17:32:53 +0800

    Complete data viewer using streamlit.

---
+++ README.md
@@ -0,0 +1,13 @@
+Kaggle Competition: cassava leaf disease detection
+
+### Introduction
+This repo contains the code to complete Kaggle Competition: [Cassava Leaf Disease Detection](https://www.kaggle.com/c/cassava-leaf-disease-classification/overview) and consists of below parts:
+#### Data Viwer
+This is a simple interactive data viewer created using [streamlit](https://www.streamlit.io/)
+To view the dataset, you need to 
+1. download dataset and unzip to `./cassava-leaf-disease-classification-data`
+2. Run `streamlit run data_viewer_streamlit.py` and visit http://localhost:8501 and there you go.
+
+#### Model training and predicting. 
+**[TBD]**
+
---
+++ data_viewer_streamlit.py
@@ -0,0 +1,60 @@
+import streamlit as st
+import os
+import pandas as pd
+import cv2
+
+LABEL_IDX_NAME_MAPPING = {
+    0: "Cassava Bacterial Blight (CBB)",
+    1: "Cassava Brown Streak Disease (CBSD)",
+    2: "Cassava Green Mottle (CGM)",
+    3: "Cassava Mosaic Disease (CMD)",
+    4: "Healthy"
+}
+
+DATA_DIR = './cassava-leaf-disease-classification-data'
+
+
+def main():
+    st.header('Casseva Leaf Disease Detection')
+    st.markdown(""""There are 5 different classes, including 4 disease labels and healthy label:
+                    1. Cassava Bacterial Blight (CBB)
+                    2. Cassava Brown Streak Disease (CBSD)
+                    3. Cassava Green Mottle (CGM)
+                    4. Cassava Mosaic Disease (CMD)
+                    5. Healthy
+    """)
+
+    train_df = pd.read_csv(os.path.join(DATA_DIR, 'train.csv'))
+    train_df.loc[:, 'label_name'] = train_df['label'].apply(lambda label_idx: LABEL_IDX_NAME_MAPPING[label_idx])
+
+    selected_label = st.selectbox('Select a class to view', list(LABEL_IDX_NAME_MAPPING.values()), 0)
+
+    # Select a label to view
+    selected_df = train_df[train_df['label_name'] == selected_label]
+
+    if selected_df is None or selected_df.shape[0] == 0:
+        st.write('No corresponding label images found in train dataset')
+        return None
+
+    selected_idx = st.slider('Choose an image to view', 0, selected_df.shape[0] - 1, 0)
+    selected_df = selected_df.reset_index(drop=True)
+    selected_img_id = selected_df.loc[selected_idx, 'image_id']
+    st.write('** {} **'.format(selected_img_id))
+    # Read and display image
+    image_path = os.path.join(os.path.abspath(DATA_DIR), 'train_images', selected_img_id)
+    img = cv2.imread(image_path, cv2.IMREAD_COLOR)
+    img = img[:, :, [2, 1, 0]]  # BGR -> RGB
+    st.image(img)
+
+    st.write('The class balance status')
+    class_num_df = train_df.groupby(by=['label_name']).agg({'image_id': 'count'})
+    st.bar_chart(class_num_df)
+
+    ### Types of diesase per image
+    disease_num_df_per_image_df = train_df.groupby(by=['image_id']).agg({'label_name': 'count'})
+    st.write(disease_num_df_per_image_df.describe())
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    main()
